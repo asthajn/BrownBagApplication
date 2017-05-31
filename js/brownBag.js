da832e676d544e5888ff62e94146bf1cbd32de50
Astha Jain <astha.jain@nextlabs.com>
2017-05-31 23:22:19 +0800

    testing server

---
+++ js/brownBag.js
@@ -0,0 +1,10 @@
+var showVegMenu = function(){
+/*	var displayText = "Aaloo Gobhi Manchurian";
+	return displayText;*/
+	document.write("Aaloo Gobhi Manchurian");
+}
+
+var showNonVegMenu = function(){
+	var displayText = "Chicken";
+	return displayText;
+}
